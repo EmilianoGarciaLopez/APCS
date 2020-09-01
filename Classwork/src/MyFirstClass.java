0fe3e3126797f974b5a1ce9a7c641139c912ff0d
Emiliano Garcia <60363956+EmilianoGarciaLopez@users.noreply.github.com>
2020-09-01 15:08:23 -0400

    S

--- Classwork/src/MyFirstClass.java
+++ Classwork/src/MyFirstClass.java
@@ -1,8 +1,8 @@
 public class MyFirstClass {
     public static void main(String[] args) {
         System.out.println("Dark theme is for the cool kids");
-        String name = "Emiliano Garcia";
-        System.out.println("My name is " + name);
+        String Name = "Emiliano Garcia";
+        System.out.println("My name is " + Name);
     }
 }
 
