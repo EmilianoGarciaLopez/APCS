f5d45afb6161440adee1e7c5f13dcc671b632638
Emiliano Garcia <60363956+EmilianoGarciaLopez@users.noreply.github.com>
2020-09-03 11:03:10 -0400

    S

--- Classwork/src/MyFirstClass.java
+++ Classwork/src/MyFirstClass.java
@@ -1,6 +1,6 @@
 public class MyFirstClass {
     public static void main(String[] args) {
-        System.out.println("Github Push Test");
+        System.out.println("Darkmode is for the cool kids");
         String Name = "Emiliano Garcia";
         System.out.println("My name is " + Name);
     }
