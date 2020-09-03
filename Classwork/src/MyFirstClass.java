23a366508d965d09c60b02f6bcb2e28e18b6916c
Emiliano Garcia <60363956+EmilianoGarciaLopez@users.noreply.github.com>
2020-09-03 11:04:17 -0400

    Update

--- Classwork/src/MyFirstClass.java
+++ Classwork/src/MyFirstClass.java
@@ -1,7 +1,7 @@
 public class MyFirstClass {
     public static void main(String[] args) {
         System.out.println("Darkmode is for the cool kids");
-        String Name = "Emiliano Garcia";
+        String Name = "Emiliano Garcia Lopez";
         System.out.println("My name is " + Name);
     }
 }
