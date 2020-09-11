b0722e76fabf1c741298f9e90822ba7a0cc1785e
Emiliano Garcia <60363956+EmilianoGarciaLopez@users.noreply.github.com>
2020-09-11 11:00:37 -0400

    Update

--- Classwork/src/HolyGrail.java
+++ Classwork/src/HolyGrail.java
@@ -5,9 +5,32 @@
  * @author emilianogarcia
 */
 
+/*
+primatives (holdover from c/c++
+int, double, boolean, char
+Objects start with a capital letter
+ */
+
+// this imports a library when I compile this file+
 import java.util.Scanner;
+
+
 public class HolyGrail {
     public static void main(String[]args){
+        Scanner in = new Scanner(System.in);
+        System.out.print("What is your name: ");
+        String name = in.nextLine();
+        System.out.println("Hello "+name+", nice to meet you!");
+        System.out.print("How old are you: ");
+        int age = in.nextInt();
+        System.out.print("How many pets do you have: ");
+        int numpets = in.nextInt();
+        System.out.println(name);
+        System.out.println("You are "+ age +" years old.");
+        System.out.println("you have "+ numpets +" pets");
+
+
 
     }
 }
+
