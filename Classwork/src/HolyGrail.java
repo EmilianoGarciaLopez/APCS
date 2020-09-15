8580cc22889d4e7a86857e61c81a6d3759232776
Emiliano Garcia <60363956+Mastersmash97@users.noreply.github.com>
2020-09-15 11:14:22 -0400

    commit

--- Classwork/src/HolyGrail.java
+++ Classwork/src/HolyGrail.java
@@ -1,9 +1,11 @@
+import java.util.Scanner;
+
 /**
  * HolyGrail.Java is the first lab i'll write. It is a simple UI program
  * that talks to the user
  * 09/15/2020
  * @author emilianogarcia
-*/
+ */
 
 /*
 primatives (holdover from c/c++
@@ -12,9 +14,6 @@ Objects start with a capital letter
  */
 
 // this imports a library when I compile this file
-import java.util.Scanner;
-
-
 public class HolyGrail {
     public static void main(String[]args){
         Scanner in = new Scanner(System.in);
