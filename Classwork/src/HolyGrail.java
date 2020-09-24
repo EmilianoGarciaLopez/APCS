d0099682973645ca5757959b95775dcdf0293938
Emiliano Garcia <60363956+Mastersmash97@users.noreply.github.com>
2020-09-24 10:12:08 -0400

    commit

--- Classwork/src/HolyGrail.java
+++ Classwork/src/HolyGrail.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 /**
  * HolyGrail.Java is the first lab i'll write. It is a simple UI program
  * that talks to the user
- * 09/15/2020
+ * @version /15/2020
  * @author emilianogarcia
  */
 
--- Classwork/src/Student.java
+++ Classwork/src/Student.java
@@ -44,7 +44,10 @@ public class Student {
     public static void main(String[] args) {
         Student s1 = new Student("PJ", "Gillispie", 11, 4.2);
         Student s2 = new Student("Abby", "Goldman", 12, 4.2);
-        Student s3 = new Student();
+
+
+        System.out.println(s1.getClass());
+        System.out.println(s1);
     }
 
 }
