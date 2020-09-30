a1b7cdd270c6d89f3c4b8ffd14c804a3f18dde1a
Emiliano Garcia <60363956+Mastersmash97@users.noreply.github.com>
2020-09-30 10:56:10 -0400

    commit

---
+++ Classwork/src/RomanToDecimal.java
@@ -0,0 +1,23 @@
+/** This program ingests a roman numeral
+ * and changes it to a base 10 number
+ * This lab is due Tuesday, November 6
+ */
+
+public class RomanToDecimal {
+    // no private data, only helper methods
+
+    /**
+     * converts a string to a valid decimal (base-10 value)
+     * @param roman Must be an uppercase string
+     * @return
+     */
+    public static int romanToDecimal(String roman)  {
+        int sum = 0;
+    }
+
+    public static void main(String[]args)   {
+        System.out.println("Input: X => output: " + romanToDecimal("x".toUpperCase()));
+
+    }
+
+}
