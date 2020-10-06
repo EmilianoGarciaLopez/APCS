653700b58f9c0a6cc01aebbbd4a2e7bc5ff5859c
Emiliano Garcia <60363956+EmilianoGarciaLopez@users.noreply.github.com>
2020-10-05 22:52:45 -0400

    Update

--- Classwork/src/RomanToDeciamal2.java
+++ Classwork/src/RomanToDeciamal2.java
@@ -1,29 +1,24 @@
+import java.util.Hashtable;
+
 /** This program ingests a roman numeral
  * and changes it to a base 10 number
  * This lab is due Tuesday, November 6
  */
 
-public class RomanToDeciamal2 {
+public class RomanToDeciamal2(String roman2) {
 
 
-    public class romanAssign {
-        int value(char s)
-        {
-            if (s == 'I')
-                return 1;
-            if (s== 'V')
-                return 5;
-            if (s == 'X')
-                return 10;
-            if (s == 'L')
-                return 50;
-            if (s == 'C')
-                return 100;
-            if (s == 'D')
-                return 500;
-            if (s == 'M')
-                return 1000;
-            return -1;
+    public int romanAssign {
+        Hashtable<Character, Integer> doutput = new Hashtable<>();
+        doutput.put('i',1);
+        doutput.put('x',10);
+        doutput.put('c',100);
+        doutput.put('m',1000);
+        doutput.put('v',5);
+        doutput.put('l',50);
+        doutput.put('d',500);
+
+        
         }
 
     }
