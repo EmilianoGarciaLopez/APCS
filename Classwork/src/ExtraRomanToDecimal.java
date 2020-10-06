b8eaf8170661b85e54ff5e613d7959d55b0093ae
Emiliano Garcia <60363956+Mastersmash97@users.noreply.github.com>
2020-10-06 11:28:35 -0400

    commit

--- Classwork/src/ExtraRomanToDecimal.java
+++ Classwork/src/ExtraRomanToDecimal.java
@@ -1,4 +1,4 @@
-import java.util.Hashtable;
+import java.util.*;
 
 /** This program ingests a roman numeral
  * and changes it to a base 10 number
@@ -8,8 +8,8 @@ import java.util.Hashtable;
 public class ExtraRomanToDecimal(String roman2) {
 
 
-    public int romanAssign;{
-        Hashtable<Character, Integer> doutput = new Hashtable<>();
+    public void romanAssign()    {
+        Hashtable<Character, Integer> doutput = new Hashtable<Character, Integer>();
         doutput.put('i',1);
         doutput.put('x',10);
         doutput.put('c',100);
@@ -17,14 +17,19 @@ public class ExtraRomanToDecimal(String roman2) {
         doutput.put('v',5);
         doutput.put('l',50);
         doutput.put('d',500);
-
-
     }
+    int romanToDec(String r) {
+        int d = 0;
+        for (int i = 0; i< r.length(); i++) {
+            int r1 = doutput.get(r.charAt(i));
 
-}
+            if (i + 1 < r.length()) {
+                int r2 = doutput.get(r.charAt(i+1));
+            }
+    }
 
     public static void main(String[] args) {
         System.out.println();
 
     }
-}
+}}
