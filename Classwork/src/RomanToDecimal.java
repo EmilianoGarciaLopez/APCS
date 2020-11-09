7b136c4b0ed1b0a06fc7b3ab8fe01015cc454c0a
Emiliano Garcia <60363956+Mastersmash97@users.noreply.github.com>
2020-11-09 10:21:44 -0500

    clowned

--- Classwork/src/RomanToDecimal.java
+++ Classwork/src/RomanToDecimal.java
@@ -1,6 +1,7 @@
 import java.util.Hashtable;
 
-/** This program ingests a roman numeral
+/**
+ * This program ingests a roman numeral
  * and changes it to a base 10 number
  * This lab is due Tuesday, November 6
  */
@@ -16,13 +17,13 @@ public class RomanToDecimal { // this is a public class that handles everything 
     public static int romanToDecimal(String roman) {
         Hashtable<Character, Integer> doutput;
         doutput = new Hashtable<>(); //dictionary that contains corresponding values
-        doutput.put('I',1); // for example I = 1
-        doutput.put('X',10);
-        doutput.put('C',100);
-        doutput.put('M',1000);
-        doutput.put('V',5);
-        doutput.put('L',50);
-        doutput.put('D',500);
+        doutput.put('I', 1); // for example I = 1
+        doutput.put('X', 10);
+        doutput.put('C', 100);
+        doutput.put('M', 1000);
+        doutput.put('V', 5);
+        doutput.put('L', 50);
+        doutput.put('D', 500);
         int d = 0; // initialize the decimal output with a value of 0
         for (int i = 0; i < roman.length(); i++) { // this traverses the string, a for loop
             if (doutput.containsKey(roman.charAt(i))) { //makes sure that index i of roman is valid input
@@ -38,37 +39,35 @@ public class RomanToDecimal { // this is a public class that handles everything 
                             d = (d + r2) - r1; // if it is lesser then i is taken from (i+1)
                             i++;
                         }
-                    }
-                    else {
+                    } else {
                         d = -1;// catches invalid input and breaks for loop
                         break;
                     }
-                }
-                else {
+                } else {
                     d = d + r1; // if it is the end of the string only i matters
                     i++;
                 }
-            }
-
-            else {
+            } else {
                 d = -1;
                 break;
             } //catches invalid input
         }
-        return d;} // returns a decimal value as output
+        return d;
+    } // returns a decimal value as output
 
 
     public static void main(String[] args) { //main method that interacts with user and takes args
-        for (String roman : args)   { //uses args as the roman string value
+        for (String roman : args) { //uses args as the roman string value
             int decimal = romanToDecimal(roman.toUpperCase()); //sets decimal equal to the output of romanToDecimal
-            System.out.print("Input: "+roman+" ==> Output: ");
-            if(decimal == -1) //checks if output is invalid
+            System.out.print("Input: " + roman + " ==> Output: ");
+            if (decimal == -1) //checks if output is invalid
                 System.out.println("invalid");
             else
                 System.out.println(decimal);
         }
         System.out.println("My extra is that this program handles non-logical input such as 'ic'.");
-    }}
+    }
+}
 
     /* sources:
     https://www.geeksforgeeks.org/convert-a-roman-number-to-decimal-using-hashmap-in-java/
