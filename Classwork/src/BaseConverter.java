2074e932ac3230eec4d1adb16dcf18a412e30827
Emiliano Garcia <60363956+EmilianoGarciaLopez@users.noreply.github.com>
2020-11-16 21:32:13 -0500

    Update

---
+++ Classwork/src/BaseConverter.java
@@ -0,0 +1,10 @@
+/**
+ * This program is a base converter that reads a data file and outputs a converted file
+ *
+ * @author emiliano
+ * @version 1
+ */
+
+//https://37217a484a23ab97c7ac-fa9b9614c621c6ddb72aebe76bae7b72.ssl.cf1.rackcdn.com/6xrrzezhxa/Proj0201_BaseConverter.pdf
+public class BaseConverter {
+}
