23092e8459cd9f2ed3b68d7305f0461f5a1a6a81
Emiliano Garcia <60363956+Mastersmash97@users.noreply.github.com>
2020-11-17 10:07:42 -0500

    bruh

--- Classwork/src/BaseConverter.java
+++ Classwork/src/BaseConverter.java
@@ -7,4 +7,35 @@
 
 //https://37217a484a23ab97c7ac-fa9b9614c621c6ddb72aebe76bae7b72.ssl.cf1.rackcdn.com/6xrrzezhxa/Proj0201_BaseConverter.pdf
 public class BaseConverter {
+
+    // Constructor for class.
+    public BaseConverter(){
+
+    }
+
+
+    // Convert a String num in fromBase to base-10 int.
+    public int strToInt(String num, String fromBase){
+        return 0;
+
+    }
+
+
+    // Convert a base-10 int to a String number of base toBase.
+    public String intToStr(int num, int toBase){
+        return "";
+
+    }
+
+
+    // Opens the file stream, inputs data one line at a time, converts, prints
+    // the result to the console window and writes data to the output stream.
+    public void inputConvertPrintWrite(){
+
+    }
+    // Main method for class BaseConverter. Yours should look just like this!
+    public static void main(String[] args) {
+        BaseConverter app = new BaseConverter();
+        app.inputConvertPrintWrite();
+    }
 }
