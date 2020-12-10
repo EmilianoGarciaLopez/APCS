1acc15431d0bd386f4dd3aa90a2b3767222211d9
Emiliano Garcia <60363956+EmilianoGarciaLopez@users.noreply.github.com>
2020-12-10 11:30:16 -0500

    update

--- Classwork/src/scratch.java
+++ Classwork/src/scratch.java
@@ -3,13 +3,13 @@ public class scratch {
     public static void main(String[] args) {
         // 1 System.out.println("C:\\dictionaries\\words.txt");
 
-        //2
-        //return ((testString.charAt(testString.length() - 1)=='*') && !(testString.equals("")));
+        //2a
+        //return s.length()>0 && s.charAt(s.length()-1) == '*';
 
         //2b
-        //return ((testString.charAt(testString.length() - 2)== "**") && testString.length >=4);
+        //return s.length()>2 && s.charAt(s.length()-2) == "**";
 
-        System.out.println(convert("05/31/2019"));
+        //4b System.out.println(convert("05/31/2019"));
 
     }
 
@@ -24,11 +24,7 @@ public class scratch {
 
         String[] newStr = dateStr.split("/");
 
-        String day = newStr[1];
-        String month = newStr[0];
-        String year = newStr[2];
-
-        return (day + "-" + month + "-" + year);
+        return (newStr[1] + "-" + newStr[0] + "-" + newStr[2]);
     }
 
 }
