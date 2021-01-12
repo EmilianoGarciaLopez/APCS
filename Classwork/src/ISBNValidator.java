25d3786ee753069e43c86e125bcbf5d022547fa7
Emiliano Garcia <60363956+Mastersmash97@users.noreply.github.com>
2021-01-12 11:27:47 -0500

    update

--- Classwork/src/ISBNValidator.java
+++ Classwork/src/ISBNValidator.java
@@ -1,13 +1,17 @@
+import java.io.File;
 import java.util.Scanner;
+
 public class ISBNValidator {
-    private String[] validNums, invalidNums;
+    private final String[] validNums;
+    private final String[] invalidNums;
 
 
     /**
      * simple constructor; initializes arrays
      */
     public ISBNValidator() {
-
+        validNums = new String[1500];
+        invalidNums = new String[500];
     }
 
 
@@ -16,17 +20,50 @@ public class ISBNValidator {
      * corresponding arrays
      */
     public void importData() {
+        Scanner in = null;
 
+        try {
+            in = new Scanner(new File("./Classwork/isbn_files/isbn1.dat"));
+            //TODO: JFile chooser
+
+            int valid = 0, invalid = 0;
+
+            while (in.hasNext()) {
+                //TODO: which array should each number be put into;
+                String isbn = in.nextLine();
+                if (isValidISBN(isbn)) {
+                    validNums[valid++] = isbn;
+                } else {
+                    invalidNums[invalid++] = isbn;
+                }
+            }
+            in.close();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
     }
 
 
     /**
      * determines validity of supplied ISBN number; called inside importData
+     *
      * @param isbn A string representing an ISBN number
      * @return true if isbn is valid
      */
     public boolean isValidISBN(String isbn) {
-        //TODO: fix this
+        String[] numArr = isbn.split("-");
+        int[] nums = new int[numArr.length];
+
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] = Integer.parseInt(numArr[i]);
+        }
+
+        if (nums[0] == 978 || nums[0] == 979) {
+            return false;
+        } else {
+            //TODO: build the check digit algorithm
+        }
+
         return false;
     }
 
@@ -40,11 +77,13 @@ public class ISBNValidator {
 
     /**
      * Main entry point for ISBN validator
+     *
      * @param args command line arguments, if needed
      */
-    public static void main(String[] args){
+    public static void main(String[] args) {
         ISBNValidator app = new ISBNValidator();
-        System.out.println("* ISBN Validator Program *"); System.out.println("...Importing data...");
+        System.out.println("* ISBN Validator Program *");
+        System.out.println("...Importing data...");
         app.importData(); // imports data from the text file app.runProgram();
         // runs using a while loop; see examples System.out.println("* End of Program *");
     }
