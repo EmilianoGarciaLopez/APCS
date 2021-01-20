f37f6977a99a3625efba505dc72f55ab0401b19a
Emiliano Garcia Lopez <emilianogarcialopez26@gmail.com>
2021-01-20 16:43:31 -0500

    javadoc for ScrabbleScorer

--- Classwork/src/ScrabbleScorer.java
+++ Classwork/src/ScrabbleScorer.java
@@ -10,16 +10,28 @@ public class ScrabbleScorer {
     private final int[] points = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
     ArrayList<String> dictionary;
 
+    /**
+     * This method initializes the dictionary arrayList
+     * and calls the buildDictionary() method
+     */
     public ScrabbleScorer() {
         dictionary = new ArrayList<>();
         buildDictionary();
     }
 
+    /**
+     * main method that handles user interaction and input
+     *
+     * @param args command line arguments, if needed
+     */
     public static void main(String[] args) {
         ScrabbleScorer app = new ScrabbleScorer();
         //TODO -- you do this
     }
 
+    /**
+     * This method builds the dictionary as an arrayList from the provided txt file
+     */
     public void buildDictionary() {
         Scanner in = null;
         try {
@@ -34,13 +46,25 @@ public class ScrabbleScorer {
         }
     }
 
+    /**
+     * This method checks if a word provided by the user is real
+     *
+     * @param word a String provided by the user
+     * @return a boolean value indicating if the supplied input is a real word
+     */
     public boolean isValidWord(String word) {
         return Collections.binarySearch(dictionary, word) >= 0;
     }
 
+    /**
+     * This method gives the appropriate point score for a user-provided word
+     *
+     * @param word a String provided by the user
+     * @return score an Int containing the point total for the provided input
+     */
     public int getWordScore(String word) {
-        //TODO -- you implement this
-        return 0;
+        int score = 0;
+        return score;
     }
 
 }
