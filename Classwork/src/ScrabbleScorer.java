386b806e008e36dbc351474396c973d4b0f35fae
Emiliano Garcia <60363956+Mastersmash97@users.noreply.github.com>
2021-01-20 11:54:13 -0500

    ch9 hw

--- Classwork/src/ScrabbleScorer.java
+++ Classwork/src/ScrabbleScorer.java
@@ -1,10 +1,13 @@
 import java.io.File;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+//TODO javadoc
 
 public class ScrabbleScorer {
 
-    private String alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    private int[] points = {1,3,3,2,1,4,2,4,1,8,5,1,3,1,1,3,10,1,1,1,1,4,4,8,4,10};
+    private final String alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    private final int[] points = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
     ArrayList<String> dictionary;
 
     public ScrabbleScorer() {
@@ -12,9 +15,13 @@ public class ScrabbleScorer {
         buildDictionary();
     }
 
+    public static void main(String[] args) {
+        ScrabbleScorer app = new ScrabbleScorer();
+        //TODO -- you do this
+    }
+
     public void buildDictionary() {
         Scanner in = null;
-
         try {
             in = new Scanner(new File("/Classwork/datafiles/SCRABBLE_WORDS.txt"));
             while (in.hasNext()) {
@@ -22,22 +29,18 @@ public class ScrabbleScorer {
             }
             in.close();
             Collections.sort(dictionary);
-        }
-        catch(Exception e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
     public boolean isValidWord(String word) {
-        //TODO
+        return Collections.binarySearch(dictionary, word) >= 0;
     }
 
     public int getWordScore(String word) {
-        //TODO
-    }
-
-    public static void main(String[] args) {    
-        ScrabbleScorer app = new ScrabbleScorer();
+        //TODO -- you implement this
+        return 0;
     }
 
 }
