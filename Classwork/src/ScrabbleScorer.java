aaefd50fc1880807c78759f90ab66d784e95e715
Emiliano Garcia <60363956+Mastersmash97@users.noreply.github.com>
2021-01-20 11:11:03 -0500

    update

--- Classwork/src/ScrabbleScorer.java
+++ Classwork/src/ScrabbleScorer.java
@@ -1,13 +1,31 @@
+import java.io.File;
 import java.util.*;
 
 public class ScrabbleScorer {
 
     private String alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     private int[] points = {1,3,3,2,1,4,2,4,1,8,5,1,3,1,1,3,10,1,1,1,1,4,4,8,4,10};
+    ArrayList<String> dictionary;
 
+    public ScrabbleScorer() {
+        dictionary = new ArrayList<>();
+        buildDictionary();
+    }
 
     public void buildDictionary() {
-        //TODO
+        Scanner in = null;
+
+        try {
+            in = new Scanner(new File("/Classwork/datafiles/SCRABBLE_WORDS.txt"));
+            while (in.hasNext()) {
+                dictionary.add(in.nextLine());
+            }
+            in.close();
+            Collections.sort(dictionary);
+        }
+        catch(Exception e) {
+            e.printStackTrace();
+        }
     }
 
     public boolean isValidWord(String word) {
@@ -16,16 +34,10 @@ public class ScrabbleScorer {
 
     public int getWordScore(String word) {
         //TODO
-
-        word = in.nextLine();
-        score = 0;
-        for(int i = 0; i < word.length(); i++) {
-            //TODO
-        }
     }
 
     public static void main(String[] args) {    
-        ScrabbleScorer app = new Scrabble Scorer();
+        ScrabbleScorer app = new ScrabbleScorer();
     }
 
 }
