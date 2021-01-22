a1ffe5a6846b1b0d5ce32d156f9374cd3ad481fd
Emiliano Garcia Lopez <emilianogarcialopez26@gmail.com>
2021-01-22 10:59:58 -0500

    added main and scoring to ScrabbleScorer

--- Classwork/src/ScrabbleScorer.java
+++ Classwork/src/ScrabbleScorer.java
@@ -1,6 +1,7 @@
 import java.io.File;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Locale;
 import java.util.Scanner;
 //TODO javadoc
 
@@ -20,22 +21,12 @@ public class ScrabbleScorer {
     }
 
     /**
-     * main method that handles user interaction and input
-     *
-     * @param args command line arguments, if needed
-     */
-    public static void main(String[] args) {
-        ScrabbleScorer app = new ScrabbleScorer();
-        //TODO -- you do this
-    }
-
-    /**
      * This method builds the dictionary as an arrayList from the provided txt file
      */
     public void buildDictionary() {
         Scanner in;
         try {
-            in = new Scanner(new File("/Classwork/datafiles/SCRABBLE_WORDS.txt"));
+            in = new Scanner(new File("Classwork/datafiles/SCRABBLE_WORDS.txt")); //your directory path is different
             while (in.hasNext()) {
                 dictionary.add(in.nextLine());
             }
@@ -53,7 +44,7 @@ public class ScrabbleScorer {
      * @return a boolean value indicating if the supplied input is a real word
      */
     public boolean isValidWord(String word) {
-        return Collections.binarySearch(dictionary, word) >= 0;
+        return (Collections.binarySearch(dictionary, word) >= 0);
     }
 
     /**
@@ -64,10 +55,37 @@ public class ScrabbleScorer {
      */
     public int getWordScore(String word) {
         int score = 0;
-        for(int i =0; i<word.length(); i++){
-            score +=points[letters.indexOf(word.charAt(i))];
+        for (int i = 0; i < word.length(); i++) {
+            score += points[letters.indexOf(word.charAt(i))];
         }
         return score;
+    }
+
+    /**
+     * main method that handles user interaction and input
+     *
+     * @param args command line arguments, if needed
+     */
+    public static void main(String[] args) {
+        ScrabbleScorer app = new ScrabbleScorer();
+        Scanner in = new Scanner(System.in);
+        System.out.println("* Welcome to the Scrabble Word Scorer app *");
+
+        while (true) {
+            //TODO -- you do this
+            System.out.print("Enter a word to score or 0 to quit:\t");
+            String temp = in.nextLine();
+            String input = temp.toUpperCase();
+
+            if (input.equals("0")) {
+                break;
+            } else if (app.isValidWord(input)) {
+                System.out.println(temp  + " = " + app.getWordScore(input) + " points");
+            } else {
+                System.out.println(temp + " is not a valid word in the dictionary");
+            }
+        }
+        System.out.println("Exiting the program thanks for playing");
     }
 
 }
